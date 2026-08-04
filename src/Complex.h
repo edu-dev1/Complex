def372fb38a15a52593a932795f37736fd48b08e
Eduardo <cogajosedu@gmail.com>
2026-08-03 21:47:06 -0600

    Create Complex.h

---
+++ src/Complex.h
@@ -0,0 +1,38 @@
+#ifndef Complex_h
+#define Complex_h
+
+#include <iostream>
+
+#define j 'j'
+
+class Complex{
+    private:
+        char __i_sign;
+        Complex(int r_num, int r_den, int i_num, int i_den); // Only for complex division.
+        int __r_num, __r_den, __i_num, __i_den;
+        bool __fraction_form;
+        char __i_sign_num;
+    public:
+        int _real;
+        int _imaginary;
+        Complex();
+        Complex(int real, int imaginary); // Creates a complex number.
+        Complex conjugate() const; // Returns it's conjugate.
+        Complex opposite() const; // Returns it's oppsited.
+        Complex operator+(const Complex &complex) const; // Add two complex numbers.
+        Complex operator-(const Complex &complex) const; // Subtract two complex numbers.
+        Complex operator-() const; // Same as the opposite method.
+        Complex operator*(const Complex &complex) const;
+        Complex operator/(const Complex &complex) const;
+        bool operator==(const Complex &complex) const;
+        bool operator!=(const Complex &complex) const;
+        bool operator>(const Complex &complex) const;
+        bool operator>=(const Complex &complex) const;
+        bool operator<(const Complex &complex) const;
+        bool operator<=(const Complex &complex) const;
+        friend std::ostream& operator<<(std::ostream& out, const Complex &complex);
+};
+
+int MCD(int a, int b);// Yes, it does what you think it does.
+
+#endif
